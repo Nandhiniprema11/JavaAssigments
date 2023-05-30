7f68789e58585a9ad07cb2a791a2380e23f5732f
Nandhiniprema11 <134316158+Nandhiniprema11@users.noreply.github.com>
2023-05-30 22:34:32 +0530

    Add files via upload

---
+++ Streameg/src/streammeg/OptionalEg.java
@@ -0,0 +1,31 @@
+package streammeg;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalEg {
+
+	public static void main(String[] args) {
+		List<String> strlist = Arrays.asList("Rat","Cat","Lion","","","Tiger","Elephant","Cheeta","Rhino"); 
+		System.out.println("List of Strings : " + strlist);
+        
+		String str = getAllStringValues(strlist);
+		System.out.println(str);
+	}
+
+	private static String getAllStringValues(List<String> strlist) {
+		Optional<String> st=   strlist.stream().filter(str->str.startsWith("R")).findFirst();
+		//Optional<String> st1=   strlist.stream().filter(str->str.startsWith("R")).f();To get second value loop is needed
+		return st.isPresent() ? st.get() : "No String Present";
+	}
+
+}
+
+
+//(expr)?true   :false
+
+
+
+
+
